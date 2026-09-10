130db252d28ddd8b2740c59986e3aa4e06895a71
cl0udsw1tch <nurein.umeya@mail.utoronto.ca>
2026-09-10 01:23:48 -0400

    test script fix (api)

--- tc-api-test.sh
+++ tc-api-test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+set -a
+source .env
+set +a
+
 POSTGRES_DB="tc_test"
 
-docker compose run --rm -e POSTGRES_DB=${POSTGRES_DB} tc-api npm run test
+docker compose run --rm \
+    -e POSTGRES_DB=${POSTGRES_DB} \
+    -e DATABASE_URL=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:${POSTGRES_PORT}/${POSTGRES_DB} \
+    tc-api sh -c "npm run migrate && npm run test"
